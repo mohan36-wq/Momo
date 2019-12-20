762aac07c8acbebf079e84d2086a2e337062b45e
Harshini Swaminathan <harshini098@gmail.com>
2019-12-20 21:56:03 +0530

    flames code in process

--- app/src/main/java/com/example/momo/FlamesActivity.java
+++ app/src/main/java/com/example/momo/FlamesActivity.java
@@ -3,6 +3,7 @@ package com.example.momo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,39 +25,37 @@ public class FlamesActivity extends AppCompatActivity {
         go.findViewById(R.id.button);
         s1=name1.getText().toString();
         s2=name2.getText().toString();
-        int i,j,count,k=0,n;
-        for(i=0;i<s1.length();i++)
-        {
-            for(j=0;j<s2.length();j++)
-            {
-                if(s1[i]==s2[j])
-                    s3[k]=s1[i];
-            }
-        }
-        k=0;
-        for(i=0;i<s3.length();i++)
-        {
-            for(j=0;j<i;j++)
-               if(s3[i]==s3[j])
-                  break;
-               if(j==i)
-                   s4[k++]=s3[i];
-        }
-        k--;
-        n=6;
-        for(i=k;n!=1;i=k+3)
-        {
-            if(i>n)
-              i=n%i;
-            j=0;
-            while(i<n)
-            {
-                s5[i]=s5[i+j];
-                j++;
-            }
-            n--;
-        }
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i, j, count, k = 0, n;
+                for (i = 0; i < s1.length(); i++) {
+                    for (j = 0; j < s2.length(); j++) {
+                        if (s1[i] == s2[j])
+                            s3[k] = s1[i];
+                    }
+                }
+                k = 0;
+                for (i = 0; i < s3.length(); i++) {
+                    for (j = 0; j < i; j++)
+                        if (s3[i] == s3[j])
+                            break;
+                    if (j == i)
+                        s4[k++] = s3[i];
+                }
+                k--;
+                n = 6;
+                for (i = k; n != 1; i = i + 3) {
+                    if (i > n)
+                        i = n % i;
+                    j = 0;
+                    while (i < n) {
+                        s5[i] = s5[i + j];
+                        j++;
+                    }
+                    n--;
+                }
 
 
-    }
-}
+            }
+        };
