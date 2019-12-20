09bdc86d0466157a36616fbfdd49797ef99bc88c
mohan36-wq <mohanavigneshh@gmail.com>
2019-12-20 20:52:08 +0530

    Added flames activity

--- app/src/main/java/com/example/momo/MainActivity.java
+++ app/src/main/java/com/example/momo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.momo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btOkay;
+    Button btOkay, btFlames;
     CheckBox chMomo, chJuju;
 
     @Override
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         btOkay = findViewById(R.id.button_okay);
         chMomo = findViewById(R.id.checkBox_momo);
         chJuju = findViewById(R.id.checkBox_juju);
+        btFlames = findViewById(R.id.button_flames);
 
         btOkay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
                 } else if(chJuju.isChecked()) {
                     Toast.makeText(MainActivity.this,"Periya Kolandhai",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btFlames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FlamesActivity.class));
             }
         });
 
