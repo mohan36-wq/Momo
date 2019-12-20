1b218e171e9162fb22406702286c12a09aa9b5c7
mohan36-wq <mohanavigneshh@gmail.com>
2019-12-20 14:43:14 +0530

    Added codings

--- app/src/main/java/com/example/momo/MainActivity.java
+++ app/src/main/java/com/example/momo/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.momo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btOkay;
+    CheckBox chMomo, chJuju;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btOkay = findViewById(R.id.button_okay);
+        chMomo = findViewById(R.id.checkBox_momo);
+        chJuju = findViewById(R.id.checkBox_juju);
+
+        btOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(chMomo.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Kutty kolandhai", Toast.LENGTH_SHORT).show();
+                } else if(chJuju.isChecked()) {
+                    Toast.makeText(MainActivity.this,"Periya Kolandhai",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
